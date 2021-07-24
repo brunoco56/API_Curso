6ca880653101d4b87f4816ed1684891b0f37e90d
Bruno <brunoco56@hotmail.com>
2021-07-24 19:14:29 -0300

    Criado Controller curso e CursoViewModelInput

---
+++ curso.api/Controllers/CursoController.cs
@@ -0,0 +1,25 @@
+﻿using curso.api.Models.Cuso;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Post(CursoViewModelInput curso)
+        {
+
+
+            return Created("", curso);
+        }
+    }
+}
