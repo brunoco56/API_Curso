9adc7b431987d94f243fbbae804a473c7d707deb
Bruno <brunoco56@hotmail.com>
2021-07-24 19:25:39 -0300

    Criado view Model Output dos Cursos

--- curso.api/Controllers/CursoController.cs
+++ curso.api/Controllers/CursoController.cs
@@ -1,9 +1,11 @@
 ﻿using curso.api.Models.Cuso;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace curso.api.Controllers
@@ -12,14 +14,37 @@ namespace curso.api.Controllers
     [ApiController]
     public class CursoController : ControllerBase
     {
-
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao Cadastrar o Curso")]
+        [SwaggerResponse(statusCode: 401, description: "Campos Obrigatórios")]
         [HttpPost]
         [Route("")]
         public async Task<IActionResult> Post(CursoViewModelInput curso)
         {
-
+            var codigoUsuario = int.Parse(User.FindFirst(c=> c.Type == ClaimTypes.NameIdentifier)?.Value);
 
             return Created("", curso);
         }
+
+
+
+
+
+
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao Obter os Cursos")]
+        [SwaggerResponse(statusCode: 401, description: "Não Autorizado")]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get()
+        {
+            var cursos = new List<CursoViewModelInput>();
+
+
+
+
+            return Ok();
+        }
+
+
+
     }
 }
