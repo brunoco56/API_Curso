a45618f26966761e557d6b5f498ff1a137a79eb5
Bruno <brunoco56@hotmail.com>
2021-07-25 00:41:02 -0300

    criação de pastas para a estrutua do código e entidade Usuario

--- curso.api/Startup.cs
+++ curso.api/Startup.cs
@@ -76,24 +76,6 @@ namespace curso.api
             };
         });
 
-
-            //services.AddAuthentication(x =>
-            //{
-            //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //}).AddJwtBearer(x =>
-            //{
-            //    x.RequireHttpsMetadata = false;
-            //    x.SaveToken = true;
-            //    x.TokenValidationParameters = new TokenValidationParameters
-            //    {
-            //        ValidateIssuerSigningKey = true,
-            //        IssuerSigningKey = new SymmetricSecurityKey(secret),
-            //        ValidateIssuer = false,
-            //        ValidateAudience = false
-            //    };
-            //});
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
